612e53c5ad7eb765ed60acbe20e4bd16e29934ff
Brandon Franks <brandonfranks@MacBookAir.attlocal.net>
2025-05-13 19:48:59 -0400

    set up the actions for handling sign in and sign out

---
+++ lib/actions/user.actions.ts
@@ -0,0 +1,33 @@
+'user server';
+
+import { signInFormSchema } from '../validators';
+import { signIn, signOut } from '@/auth';
+import { isRedirectError } from 'next/dist/client/components/redirect-error';
+
+// sign in the user with credentials
+export async function signInWithCredentials(
+  previousState: unknown,
+  formData: FormData
+) {
+  try {
+    const user = signInFormSchema.parse({
+      email: formData.get('email'),
+      password: formData.get('password'),
+    });
+
+    await signIn('credentials', user);
+
+    return { success: true, message: 'Signed In Successfully' };
+  } catch (error) {
+    if (isRedirectError(error)) {
+      throw error;
+    }
+
+    return { success: false, mesage: 'Invalid email or password' };
+  }
+}
+
+// sign user out
+export async function signOutUser() {
+  await signOut();
+}
