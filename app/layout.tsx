c14d828862844b49d3d486e467b781d87b5f0826
Brandon Franks <brandonfranks@MacBookAir.attlocal.net>
2025-05-19 20:42:54 -0400

    set up the add to cart button and the toasts for adding to cart

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,10 +1,11 @@
-import type { Metadata } from "next";
-import { ThemeProvider } from "next-themes";
-import { Inter } from "next/font/google";
-import { APP_DESC, APP_NAME, SERVER_URL } from "@/lib/constants";
-import "@/assets/styles/globals.css";
+import type { Metadata } from 'next';
+import { ThemeProvider } from 'next-themes';
+import { Inter } from 'next/font/google';
+import { APP_DESC, APP_NAME, SERVER_URL } from '@/lib/constants';
+import '@/assets/styles/globals.css';
+import { Toaster } from '@/components/ui/sonner';
 
-const inter = Inter({ subsets: ["latin"] });
+const inter = Inter({ subsets: ['latin'] });
 
 export const metadata: Metadata = {
   title: {
@@ -21,15 +22,16 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en" suppressHydrationWarning>
+    <html lang='en' suppressHydrationWarning>
       <body className={`${inter.className} antialiased`}>
         <ThemeProvider
-          attribute="class"
-          defaultTheme="dark"
+          attribute='class'
+          defaultTheme='dark'
           enableSystem
           disableTransitionOnChange
         >
           {children}
+          <Toaster />
         </ThemeProvider>
       </body>
     </html>
